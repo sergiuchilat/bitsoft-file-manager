730b24e1238d314bc1f87940ae4d1fc6f50d09fc
Vadim Poscoi <vadim.poscoi@icdsoft.ro>
2024-05-24 10:15:53 +0300

    Add check ip middleware

--- src/app/middleware/index.ts
+++ src/app/middleware/index.ts
@@ -1,3 +1,23 @@
-export default [
+import { ProcessIpMiddleware } from '@/app/middleware/middlewares/process-ip.middleware';
+import { ClassicAuthController } from '@/app/modules/auth/classic-auth/classic-auth.controller';
+import { PassportJsController } from '@/app/modules/auth/passport-js/passport-js.controller';
+import { ParseTokenMiddleware } from '@/app/middleware/middlewares/parse-token.middleware';
 
+export default [
+  {
+    guard: ParseTokenMiddleware,
+    routes: ClassicAuthController,
+  },
+  {
+    guard: ParseTokenMiddleware,
+    routes: PassportJsController,
+  },
+  {
+    guard: ProcessIpMiddleware,
+    routes: ClassicAuthController,
+  },
+  {
+    guard: ProcessIpMiddleware,
+    routes: PassportJsController,
+  },
 ];
