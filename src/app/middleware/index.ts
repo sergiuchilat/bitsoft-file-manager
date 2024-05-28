5c0c5c811460d9a90f4d4815bed724327d906088
vicug <vicugaideic@gmail.com>
2024-05-28 14:16:16 +0300

    connected parse localization middleware for all routes

--- src/app/middleware/index.ts
+++ src/app/middleware/index.ts
@@ -2,11 +2,16 @@ import { IpFilterMiddleware } from '@/app/middleware/middlewares/ip-filter.middl
 import { ClassicAuthController } from '@/app/modules/auth/classic-auth/classic-auth.controller';
 import { PassportJsController } from '@/app/modules/auth/passport-js/passport-js.controller';
 import { ParseTokenMiddleware } from '@/app/middleware/middlewares/parse-token.middleware';
+import {ParseLocalizationMiddleware} from '@/app/middleware/middlewares/parse-localization.middleware';
 
 export default [
   {
     guard: ParseTokenMiddleware,
     routes: ClassicAuthController,
+  },
+  {
+    guard: ParseLocalizationMiddleware,
+    routes: '*',
   },
   {
     guard: ParseTokenMiddleware,
