3eaefd63cc3ae18eac66259d319a74d45e8968e5
gluha228 <alexander.glushcov@gmail.com>
2024-06-06 22:46:26 +0300

    adjusted dto

--- src/app/modules/file-manager/dtos/upload-file.request.dto.ts
+++ src/app/modules/file-manager/dtos/upload-file.request.dto.ts
@@ -1,33 +1,74 @@
 import { StorageProviderName } from '@/app/modules/file-manager/storage-providers/enums/storage.provider.name.enum';
 import { AccessLevel } from '@/app/modules/file-manager/access-management/enums/access-level.enum';
-import { AccessType } from '@/app/modules/file-manager/access-management/enums/access-type.enum';
-import { UUID } from 'typeorm/driver/mongodb/bson.typings';
-import { Expose } from 'class-transformer';
+import { Expose, Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsEnum, IsString } from 'class-validator';
+import { IsArray, IsBoolean, IsDate, IsEnum, IsString, IsUUID, ValidateNested } from 'class-validator';
 
-export class UploadFileRequestDto {
-  @Expose()
-  @IsString()
-  name: string;
-  @Expose()
-  @IsEnum(StorageProviderName)
-  provider: StorageProviderName;
-  @Expose()
-  @IsString()
-  folder: string;
-  @Expose()
-  @IsEnum(AccessLevel)
-  accessLevel: AccessLevel;
-  @Expose()
-  @IsEnum(AccessType)
-  accessType: AccessType;
-  @Expose()
+export class BasicAccessDTO {
   @ApiProperty({
     example: '2024-05-24T04:08:06.013Z',
   })
-  accessUntil: Date;
+  @IsDate()
+  read?: Date;
+  @ApiProperty({
+    example: '2024-05-24T04:08:06.013Z',
+  })
+  @IsDate()
+  write?: Date;
+}
+
+export class UserAccessDTO {
+  @IsUUID()
+  @ApiProperty()
+  user_uuid: string;
+  @IsEnum(AccessLevel)
+  @ApiProperty()
+  level: AccessLevel;
+  @IsUUID()
+  @ApiProperty()
+  until?: Date;
+}
+
+export class AccessDTO {
+  @IsBoolean()
+  @ApiProperty()
+  public: boolean;
+  @ApiProperty({type: [BasicAccessDTO]})
+  @ValidateNested()
+  @Type(() => BasicAccessDTO)
+  link: BasicAccessDTO;
+  @ApiProperty({type: [BasicAccessDTO]})
+  @ValidateNested()
+  @Type(() => BasicAccessDTO)
+  authorized: BasicAccessDTO;
+  @ApiProperty({type: [UserAccessDTO]})
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => UserAccessDTO)
+  user: UserAccessDTO[];
+}
+
+export class UploadFileRequestDto {
+  //todo blob content
   @Expose()
-  @IsArray() //todo use group validations so allowedUsers are not empty if accessType is User
-  allowedUsers: UUID[]
+  @ApiProperty()
+  @IsString()
+  name: string;
+  @Expose()
+  @ApiProperty()
+  @IsUUID()
+  owner: string;
+  @Expose()
+  @ApiProperty()
+  @IsEnum(StorageProviderName)
+  provider: StorageProviderName;
+  @Expose()
+  @ApiProperty()
+  @IsString() //todo validation pattern
+  folder: string;
+  @Expose()
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => AccessDTO)
+  access: AccessDTO
 }
